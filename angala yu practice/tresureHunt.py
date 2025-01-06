1abe4abfa447833beda06f88bc9cd325f7310e13
prateek verma <connectprateekverma@outlook.com>
2025-01-06 17:19:05 +0530

    office1

--- angala yu practice/tresureHunt.py
+++ angala yu practice/tresureHunt.py
@@ -1,30 +1,38 @@
-print("Welcome to treasure island\nyour mission is to find the treasure")
+# print("Welcome to treasure island\nyour mission is to find the treasure")
 
-firstchoice = input("you are at a cross road where do you want to go?\nType 'left' or 'right': ")
-choise1 = ["left", "right"]
-if firstchoice not in choise1:
-        print("you cohoose invalid option")
-else:
-    if firstchoice.lower() == "left":
-        print("you have come to a lake.\nthere is an island in the middle of the lake")
-        secondchoice = input("do you want to swim to the island or wait? Type 'swim' or 'wait': ")
-        choise2 = ["swim", "wait"]
-        if secondchoice not in choise2:
-            print("you cohoose invalid option")
-        else:
-            if secondchoice.lower() == 'wait':
-                print("you come to the doorstep island,\nopen the door.")
-                thirdchoice = input("Which door do you want to choose: 'red','blue','yellow':\n ")
-                choise3 = ["red", "blue", "yellow"]
-                if thirdchoice not in choise3:
-                    print("you have not chosen a valid door")
-                else:
-                    if thirdchoice.lower() == 'red'and'blue':
-                        print("Game over")
-                    else:
-                        print("you found the treasure!")
-            else:
-                print("Game over")
-    else:
-        print("Game Over")
+# firstchoice = input("you are at a cross road where do you want to go?\nType 'left' or 'right': ")
+# choise1 = ["left", "right"]
+# if firstchoice not in choise1:
+#         print("you cohoose invalid option")
+# else:
+#     if firstchoice.lower() == "left":
+#         print("you have come to a lake.\nthere is an island in the middle of the lake")
+#         secondchoice = input("do you want to swim to the island or wait? Type 'swim' or 'wait': ")
+#         choise2 = ["swim", "wait"]
+#         if secondchoice not in choise2:
+#             print("you cohoose invalid option")
+#         else:
+#             if secondchoice.lower() == 'wait':
+#                 print("you come to the doorstep island,\nopen the door.")
+#                 thirdchoice = input("Which door do you want to choose: 'red','blue','yellow':\n ")
+#                 choise3 = ["red", "blue", "yellow"]
+#                 if thirdchoice not in choise3:
+#                     print("you have not chosen a valid door")
+#                 else:
+#                     if thirdchoice.lower() == 'red'and'blue':
+#                         print("Game over")
+#                     else:
+#                         print("you found the treasure!")
+#             else:
+#                 print("Game over")
+#     else:
+#         print("Game Over")
         
+
+print(f"this is isn't a number")
+print('This is "prateek"')
+print('''
+hi am hungry
+i am from hungry
+am going to hungry
+''')
